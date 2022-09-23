10310841d480834468abc31d886cab1f5cac307f
dennisahlqvist <dennisahlqvist@hotmail.com>
2022-09-23 03:04:09 +0300

    DAO Contract

---
+++ sophomore/DAO-Tutorial/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x66dDB4BD6CB1724f2090AB78eA61004894fec8C6";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
